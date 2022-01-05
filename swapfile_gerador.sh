42270e0f16d47dde6525c8d0c0e64c33e564d2bd
Fernando Isnaldo Silva de Faria <33297740+fernandoisnaldo@users.noreply.github.com>
2022-01-05 19:31:00 -0300

    Update swapfile_gerador.sh

--- swapfile_gerador.sh
+++ swapfile_gerador.sh
@@ -1,25 +1,21 @@
 #! /bin/bash
-echo "Copiando o fstab para a home de: "
-whoami
-cp /etc/fstab ~/fstab.bkp
-ls ~
+echo "Em que diretório você deseja que o arquivo swap esteja presente? (eg: ~/swapfile)"
+read swapfile
 echo "A partição onde fica /swapfile é Btrfs? (Y/n) "
 read btr
-if [!$btr]; then
+if [!($btr)]; then
     echo "Opção inválida"
     exit 1
 elif [$btr = "y" || $btr = "Y"]; then
-    sudo touch /swapfile
-    sudo truncate -s 0 /swapfile
-    sudo chattr +C /swapfile
-    sudo btrfs property set /swapfile compression none
+    sudo touch $swapfile
+    sudo truncate -s 0 $swapfile
+    sudo chattr +C $swapfile
+    sudo btrfs property set $swapfile compression none
 fi
 echo "Informe o tamanho do arquivo swap em MB: "
 read tamanho
-sudo dd if=/dev/zero of=/swapfile bs=1M count=$tamanho status=progress
-sudo chmod 600 /swapfile
-sudo mkswap /swapfile
-sudo swapon /swapfile
-sudo echo "# Entrada de swapfile gerada por swapfile_generator.sh" >> /etc/fstab
-sudo echo "/swapfile none swap defaults 0 0" >> /etc/fstab
+sudo dd if=/dev/zero of=$swapfile bs=1M count=$tamanho status=progress
+sudo chmod 600 $swapfile
+sudo mkswap $swapfile
+sudo swapon $swapfile
 exit 0
