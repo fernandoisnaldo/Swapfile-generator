bf3179ef55c6c9aaf160312d96334584e07c8da8
Fernando Isnaldo Silva de Faria <33297740+fernandoisnaldo@users.noreply.github.com>
2022-01-05 18:01:47 -0300

    Add files via upload

---
+++ swapfile_generator.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+echo "Copiando o fstab para a home de: "
+whoami
+cp /etc/fstab ~/fstab.bkp
+ls ~
+sudo su
+echo "A partição onde fica /swapfile é Btrfs? (Y/n) "
+read btrfs
+if [!$btrfs]; then
+    echo "Opção inválida"
+    exit 1
+elif [$btrfs = "y" || $btrfs = "Y"];
+    touch /swapfile
+    truncate -s 0 /swapfile
+    chattr +C /swapfile
+    btrfs property set /swapfile compression none
+fi
+echo “Informe o tamanho do arquivo swap em MB: ”
+read tamanho
+dd if=/dev/zero of=/swapfile bs=1M count=$tamanho status=progress
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+echo "# Entrada de swapfile gerada por swapfile_generator.sh" >> /etc/fstab
+echo "/swapfile none swap defaults 0 0" >> /etc/fstab
+exit 0
