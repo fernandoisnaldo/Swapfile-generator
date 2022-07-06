c11eddde37f698ed2da5333cae709ebc61325254
Fernando Isnaldo Silva de Faria <33297740+fernandoisnaldo@users.noreply.github.com>
2022-07-06 00:07:41 -0300

    Update swapfile_gerador.sh
    Simplified source and fix bugs

--- swapfile_gerador.sh
+++ swapfile_gerador.sh
@@ -1,23 +1,14 @@
 #! /bin/bash
-echo "Em que diretório você deseja que o arquivo swap esteja presente? (eg: ~/swapfile)"
+echo "Em que diretório completo você deseja que o arquivo swap esteja presente? (eg: /swapfile)"
 read swapfile
 sudo touch $swapfile
-echo "A partição onde fica /swapfile é Btrfs? (Y/n) "
-read btr
-if [!$btr]; then
-    echo "Opção inválida"
-    exit 1
-elif [$btr = "y" || $btr = "Y"]; then
-    sudo truncate -s 0 $swapfile
-    sudo chattr +C $swapfile
-    sudo btrfs property set $swapfile compression none
-fi
+sudo chattr +C $swapfile > /dev/null;
 echo "Informe o tamanho do arquivo swap em MB: "
 read tamanho
 sudo dd if=/dev/zero of=$swapfile bs=1M count=$tamanho status=progress
 sudo chmod 600 $swapfile
 sudo mkswap $swapfile
 sudo swapon $swapfile
-echo "Dica: Caso você queira reutilizar o arquivo swap na próxima inicialização, adicione-o no fstab."
-echo "Caso só queira reutiliza-lo em outra eventualidade, o comando é swapon /path/to/dir"
+echo "Dica: Você pode adicionar linha no fstab, caso deseje que o swapfile persista após reinicialização, por meio do seguinte comando:"
+echo "echo \"$swapfile none swap defaults 0 0\" >> /etc/fstab"
 exit 0
